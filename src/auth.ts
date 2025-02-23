bde6d288064df8c3f353f9d547e3cb2da804746a
Ajesh Jhalli <ajeshjhalli@mail.com>
2025-02-23 21:19:59 +0000

    first commit

---
+++ src/auth.ts
@@ -0,0 +1,129 @@
+import {
+	encodeBase32LowerCaseNoPadding,
+	encodeHexLowerCase,
+} from "npm:@oslojs/encoding";
+import { sha256 } from "npm:@oslojs/crypto/sha2";
+import { encodeHex } from "jsr:@std/encoding/hex";
+import { nanoid } from "https://deno.land/x/nanoid@v3.0.0/mod.ts";
+
+const kv = await Deno.openKv();
+
+console.log(await Array.fromAsync(kv.list({ prefix: [] })))
+
+// (await Array.fromAsync(kv.list({ prefix: [] }))).forEach(async (record) => {
+// 	await kv.delete(record.key);
+// });
+
+export async function createUser(username: string, password: string): Promise<User> {
+	const data = new TextEncoder().encode(password);
+	const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+	const passwordHash = encodeHex(hashBuffer);
+
+	const user: User = {
+		id: nanoid(),
+		username,
+		passwordHash,
+	};
+
+	await kv.atomic()
+		.set(["users", "id", user.id], user)
+		.set(["users", "username", user.username], user)
+		.commit();
+
+	return user;
+}
+
+export async function userExists(username: string): Promise<boolean> {
+	const userRecord = await kv.get<User>(["users", "username", username]);
+	return Boolean(userRecord.versionstamp);
+}
+
+export async function getUser(username: string): Promise<User | null> {
+	const userRecord = await kv.get<User>(["users", "username", username]);
+	return userRecord.value;
+}
+
+export function generateSessionToken(): string {
+	const bytes = new Uint8Array(20);
+	crypto.getRandomValues(bytes);
+	const token = encodeBase32LowerCaseNoPadding(bytes);
+	return token;
+}
+
+export async function createSession(
+	token: string,
+	userId: string,
+): Promise<Session> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+
+	const session: Session = {
+		id: sessionId,
+		userId: userId,
+		expiresAt: Date.now() + 1000 * 60 * 60 * 24 * 30,
+	};
+
+	await kv.set(["sessions", session.id], session);
+
+	return session;
+}
+
+export async function validateSessionToken(
+	token: string,
+): Promise<SessionValidationResult> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+
+	console.log(sessionId);
+
+	const sessionRecord = await kv.get<Session>(["sessions", sessionId]);
+
+	if (!sessionRecord.versionstamp) {
+		console.log("No timestamp");
+		return { session: null, user: null };
+	}
+
+	const session = sessionRecord.value;
+
+	if (Date.now() >= session.expiresAt) {
+
+		console.log("Token expired");
+
+		await kv.delete(["sessions", session.id]);
+		return { session: null, user: null };
+	}
+
+	const userRecord = await kv.get<User>(["users", "id", session.userId]);
+
+	if (!userRecord.versionstamp) {
+		console.log("User not found");
+		return { session: null, user: null };
+	}
+
+	const user: User = userRecord.value;
+
+	if (Date.now() >= session.expiresAt - 1000 * 60 * 60 * 24 * 15) {
+		session.expiresAt = Date.now() + 1000 * 60 * 60 * 24 * 30;
+		await kv.set(["sessions", session.id], session);
+	}
+
+	return { session, user };
+}
+
+export async function invalidateSession(sessionId: string) {
+	await kv.delete(["sessions", sessionId]);
+}
+
+export type SessionValidationResult =
+	| { session: Session; user: User }
+	| { session: null; user: null };
+
+export interface Session {
+	id: string;
+	userId: string;
+	expiresAt: number;
+}
+
+export interface User {
+	id: string;
+	username: string;
+	passwordHash: string;
+}
---
+++ src/main.ts
@@ -0,0 +1,162 @@
+// @ts-types="npm:@types/express"
+import express, { response } from "npm:express";
+import { body, query, validationResult } from "npm:express-validator";
+// @ts-types="npm:@types/body-parser"
+import bodyParser from "npm:body-parser";
+// @ts-types="npm:@types/pug"
+import pug from "npm:pug";
+import {
+  createSession,
+  createUser,
+  generateSessionToken,
+  getUser,
+  userExists,
+  validateSessionToken,
+} from "./auth.ts";
+import cookieParser from "npm:cookie-parser";
+import { encodeHex } from "jsr:@std/encoding/hex";
+
+// Users
+// Bookings
+// Calendar
+// Xero Integration
+// Assets
+// Configurable questions
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const templateIndex = pug.compileFile("./src/templates/index.pug");
+const templateSignIn = pug.compileFile("./src/templates/signin.pug");
+const templateSignUp = pug.compileFile("./src/templates/signup.pug");
+const templateProfile = pug.compileFile("./src/templates/profile.pug");
+const templateBookings = pug.compileFile("./src/templates/bookings.pug");
+
+app.get("/", async (req, res) => {
+  const { user } = await validateSessionToken(req.cookies.token);
+
+  if (user) {
+    res.redirect("/bookings");
+    return;
+  }
+
+  res.send(templateIndex());
+});
+
+app.get("/bookings", async (req, res) => {
+  const { user } = await validateSessionToken(req.cookies.token);
+
+  if (!user) {
+    res.redirect("/signin");
+    return;
+  }
+
+  res.send(
+    templateBookings({ session: req.cookies.token, username: user?.username }),
+  );
+});
+
+app.get("/profile", async (req, res) => {
+  const { user } = await validateSessionToken(req.cookies.token);
+
+  if (!user) {
+    res.redirect("/signin");
+    return;
+  }
+
+  res.send(
+    templateProfile({ session: req.cookies.token, username: user?.username }),
+  );
+});
+
+app.get("/signin", (req, res) => {
+  res.send(templateSignIn());
+});
+
+app.get("/signup", (req, res) => {
+  res.send(templateSignUp());
+});
+
+app.post("/signin", async (req, res) => {
+  const username = req.body.username;
+
+  const user = await getUser(username);
+
+  if (!user) {
+    res.status(400).send("Invalid username or password");
+    return;
+  }
+
+  const password = req.body.password;
+
+  const data = new TextEncoder().encode(password);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const passwordHash = encodeHex(hashBuffer);
+
+  if (user?.passwordHash !== passwordHash) {
+    res.status(400).send("Invalid username or password");
+    return;
+  }
+
+  const token = generateSessionToken();
+  await createSession(token, user.id);
+
+  res
+    .writeHead(200, {
+      "Set-Cookie": `token=${token}; HttpOnly`,
+      "Access-Control-Allow-Credentials": "true",
+    })
+    .redirect("/bookings");
+});
+
+app.post("/signout", (req, res) => {
+  res
+    .writeHead(200, {
+      "Set-Cookie":
+        `token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`,
+      "Access-Control-Allow-Credentials": "true",
+    })
+    .redirect("/signin");
+});
+
+app.post("/signup", [
+  body("username").notEmpty().custom(async (value, { req, loc, path }) => {
+    if (await userExists(value)) {
+      throw new Error("A user with this username already exists");
+    } else {
+      return value;
+    }
+  }),
+  body("password").notEmpty(),
+  body("passwordConfirmation").custom((value, { req, loc, path }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords don't match");
+    } else {
+      return value;
+    }
+  }),
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await createUser(username, password);
+  const token = generateSessionToken();
+  await createSession(token, user.id);
+
+  res
+    .writeHead(200, {
+      "Set-Cookie": `token=${token}; HttpOnly`,
+      "Access-Control-Allow-Credentials": "true",
+    })
+    .redirect("/bookings");
+});
+
+app.listen(8000);
